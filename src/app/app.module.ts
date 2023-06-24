ed4e3e1c4e985e2958e4d09c1101537e0cf8bbe2
Vice Dev <developvice@gmail.com>
2023-06-24 00:22:55 -0500

    first commit mi portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
